07199cf01023065b74d3d67facbc54dbc2937832
tobiasbaum <tbaum@tntinteractive.de>
2022-08-27 15:21:29 +0200

    Testdateien für Postbox

---
+++ validPostboxOrigins.js
@@ -0,0 +1,10 @@
+const validPostboxDocservers = Object.freeze([
+  'http://localhost:4200',
+  'http://localhost:4711',
+  'https://localhost:4200',
+  'https://localhost:4711'
+]);
+const validPostboxKeyservers = Object.freeze([
+  'http://localhost:4545',
+  'https://localhost:4545'
+]);
